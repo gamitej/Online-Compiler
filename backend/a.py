6075152dea4cccfa1701b947070f745507695cb2
gamitej <gamitejpratapsingh998@gmail.com>
2023-04-10 22:14:01 +0530

    flask - react coding platform done

--- backend/a.py
+++ backend/a.py
@@ -3,9 +3,6 @@ sys.stdin=open('input.txt','r')
 
 try:
 # Enter your code here
-	n = int(input())
-	for i in range(n):
-	    for j in range(i,-1,-1):
-	        print("*")
+	print("hi")
 except Exception as e:
 	print([e,'error while compling'])
--- backend/server.py
+++ backend/server.py
@@ -1,52 +1,31 @@
 import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import subprocess
 
 app = Flask(__name__)
 CORS(app)
 
-# ================ TO RUN THE INPUT.TXT WITH PYTHON FILE ====================
-content = "import sys\nsys.stdin=open('input.txt','r')\n\ntry:\n"
-error = "\nexcept Exception as e:\n\tprint([e,'error while compling'])"
-
-
-def fileReadWrite(code, input):
-    try:
-        fileName = ['a.py', 'input.txt']
-        fileInput = [content+code+error, input]
-        for i in range(2):
-            f = open(fileName[i], 'w')
-            f.write(fileInput[i])
-            f.close()
-        os.system("python3 a.py > output.txt")
-        f = open("output.txt", "r")
-        res = f.read()
-        if len(res) == 2 and res[1] == 'error while compling':
-            return res[0], False
-        return res, True
-    except Exception as e:
-        print(e)
-        return e, False
-
-
 @app.route('/compile', methods=["POST"])
 def compile():
-    try:
         req = request.get_json()
-        code, input = req['code'].replace("\n", "\n\t"), req['input']
-        if code:
+        code, input = req['code'], req['input']
 
-            res, type = fileReadWrite(code, input)
-            if type:
-                return jsonify({"output": res, "status": 'Success'}), 200
-            return jsonify({"output": res, "status": 'Error'}), 200
+        # Compile and execute the code using subprocess
+        try:
+            result = subprocess.run(['python3', '-c', code], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        except Exception as e:
+            return jsonify(output=str(e))
 
-        return jsonify({"data": "Something Went Wrong"}), 400
-
-    except Exception as e:
-        print(e)
-        return jsonify({"msg": "Something went wrong"}), 500
-
+        # Return the output as a JSON response
+        output = result.stdout + result.stderr
+        return jsonify(output=output)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
+
+'''
+this is to run file from python :-
+result = subprocess.run(['python', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+'''
