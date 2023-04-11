b14be52f3bc4f9ab61f0a97603c9b8d8432f20f1
gamitej <gamitejpratapsingh998@gmail.com>
2023-04-11 18:23:07 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,97 +1,23 @@
-import React, { useState } from "react";
+import React, { Suspense, lazy } from "react";
 
-// libs
-import Axios from "axios";
-import Editor from "react-monaco-editor";
+import HashLoader from "react-spinners/HashLoader";
 
-// comps
-import Navbar from "./comp/Navbar/Navbar";
-import CodingScreen from "./comp/Screen/ComplierScreen";
 
-function App() {
-  const [state, setState] = useState({
-    userLang: "python",
-    userTheme: "vs-dark",
-    fontSize: 20,
-    userCode: "",
-    userInput: "",
-    userOutput: "",
-    loading: "",
-  });
 
-  // Loading state variable to show spinner
-  const [loading, setLoading] = useState(false);
+const Home = lazy(() => import("./pages/Home.jsx"));
 
-  // Function to call the compile endpoint
-  const compile = () => {
-    setLoading(true);
-    if (state.userCode === ``) {
-      setLoading(false);
-      return;
-    }
+const comp = (
+  <div className="w-full h-[90vh] font-semibold text-2xl flex justify-center items-center">
+    <HashLoader color="#36d7b7" size={50} />
+  </div>
+);
 
-    // Post request to compile endpoint
-    Axios.post(`http://127.0.0.1:5000/compile`, {
-      code: state.userCode,
-      language: "py",
-      input: state.userInput,
-    }).then((res) => {
-      handleChange(res.data.output, "userOutput");
-      setLoading(false);
-    });
-  };
-
-  const btnClass =
-    "inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out";
-
-  const handleChange = (value, name) => {
-    setState({ ...state, [name]: value });
-  };
-
-  const screenProps = {
-    handleChange,
-    userOutput: state.userOutput,
-    compile,
-    loading,
-  };
-
-  const options = {
-    fontSize: state.fontSize,
-  };
+const App = () => {
   return (
-    <div className="flex flex-col h-[100vh] w-full">
-      <div className="w-full h-[100vh]">
-        <div className="h-[10vh]">
-          <Navbar
-            userLang={state.userLang}
-            userTheme={state.userTheme}
-            fontSize={state.fontSize}
-            btnClass={btnClass}
-            compile={compile}
-            handleChange={handleChange}
-          />
-        </div>
-        <div className="w-full flex h-[90vh]">
-          <div className="w-[100%]">
-            <Editor
-              options={options}
-              width="100%"
-              theme={state.userTheme}
-              language={state.userLang}
-              defaultLanguage="python"
-              defaultValue="# Enter your code here"
-              onChange={(value) => {
-                handleChange(value, "userCode");
-              }}
-            />
-          </div>
-          <div className="w-[40%]">
-            <CodingScreen {...screenProps} />
-          </div>
-        </div>
-      </div>
-    </div>
+    <Suspense fallback={comp}>
+      <Home />
+    </Suspense>
   );
-}
+};
 
 export default App;
